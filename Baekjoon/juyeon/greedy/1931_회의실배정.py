c33cfda09f5603aa21f8488277f830335cabc6a6
주연 <lpjy1107@gmail.com>
2022-11-27 01:50:41 +0900

    Baekjoon_1541,1931_회의실배정,잃어버린괄호

---
+++ Baekjoon/juyeon/greedy/1931_회의실배정.py
@@ -0,0 +1,18 @@
+N = int(input())
+time = []
+for _ in range(N):
+    a = list(map(int, input().split()))
+    time.append(a)
+
+
+time = sorted(time, key=lambda x: x[0])
+time = sorted(time, key=lambda x: x[1])
+
+print(time)
+cnt = 0
+ltime = 0
+for i, j in time:
+    if i >= ltime:
+        cnt += 1
+        ltime = j
+print(cnt)
