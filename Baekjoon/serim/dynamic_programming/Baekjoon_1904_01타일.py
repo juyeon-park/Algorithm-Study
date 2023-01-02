db1d3cdaf0d00b9061190eb1f041ecb47ae63780
Serim <37644153+ChaeSeRim@users.noreply.github.com>
2023-01-02 10:14:24 +0900

    Update Baekjoon_1904_01타일.py

--- Baekjoon/serim/dynamic_programming/Baekjoon_1904_01타일.py
+++ Baekjoon/serim/dynamic_programming/Baekjoon_1904_01타일.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sys
 
 n = int(sys.stdin.readline())
@@ -11,17 +10,3 @@ for i in range(3, n+1):
     dp[i] = (dp[i-2] + dp[i-1]) % 15746
 
 print(dp[n])
-=======
-import sys
-
-n = int(sys.stdin.readline())
-
-dp = [0]*1000001
-dp[1] = 1
-dp[2] = 2
-
-for i in range(3, n+1):
-    dp[i] = (dp[i-2] + dp[i-1]) % 15746
-
-print(dp[n])
->>>>>>> 81c810962fd8e2745b26aa72cbbaad4642f7d3cf
